7c2de43d80e2873ef692584aa4bc6fb1cb8f04dd
Dimitri Chenet <dchenet1017@gmail.com>
2021-09-25 12:44:05 -0500

    commit tree 67e0d844b2e65ae24537c478b364964791acb9ea

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+
+function findMatching(names,driverName){
+    
+    return names.filter( (n)   => {
+        console.log(n)
+       return driverName === n 
+
+    }
+ )
+}
+
+function fuzzyMatch (names, twoLetters){
+       return names.filter( (bar) =>{
+           return  bar.startsWith(twoLetters)
+        }
+
+        )
+}
+
